f36cf1e4ff0a11497e2fb48178188cdc0aef4fb3
Alex Coy <88282738+CaptainCoy@users.noreply.github.com>
2021-11-26 13:43:10 -0700

    commit

--- todo_app/JavaScript/todo.js
+++ todo_app/JavaScript/todo.js
@@ -15,7 +15,7 @@ function get_todos() {
 /* this function adds inputed tasks to get_todos function array */
 function add() {
     /* this creates variables out of inputed tasks */
-    var task = document.getElementById('task').ariaValueMax;
+    var task = document.getElementById('task').value;
 
     var todos = get_todos();
     /* this adds a new task to the end of the array */
@@ -36,7 +36,7 @@ function show() {
     var html = '<ul>';
     /* this displays tasks in order of input */
     for (var i = 0; i < todos.length; i++) {
-        html += '<li>' + todos[i] + '<button class="remove" id="'+ i + '">x</button></li>';
+        html += '<li>' + todos[i] + '<button class="remove" id="'+ i + '">&#128077;</button></li>';
     };
     html += '</ul>';
     /* this displays the task as a list */
@@ -46,3 +46,22 @@ function show() {
 document.getElementById('add').addEventListener('click', add);
 /* this will keep the inputs displayed permanently on screen */
 show();
+
+/* this tells the browser how to display the todo array after an item has been removed */
+var buttons = document.getElementsByClassName('remove');
+for (var i = 0; i < buttons.length; i++)  {
+    buttons[i].addEventListener('click', remove);
+};
+
+/* this function removes todo items from the array */
+function remove() {
+    var id = this.getAttribute('id');
+    var todos = get_todos();
+    todos.splice(id, 1);
+    localStorage.setItem('todo', JSON.stringify(todos));
+    /* this looks in the show() hoiw to display a removed item on the screen */
+    show();
+
+    return false;
+}
+
